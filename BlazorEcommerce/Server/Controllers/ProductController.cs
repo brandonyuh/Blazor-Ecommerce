5d559cd0f6cc54a397beb4fc6401a4bbc05f2815
Brandon <brandonyuh@gmail.com>
2023-06-04 18:36:09 -0700

    Get book list from backend instead of hardcoded frontend

--- BlazorEcommerce/Server/Controllers/ProductController.cs
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BlazorEcommerce.Shared;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorEcommerce.Server.Controllers
@@ -7,5 +8,36 @@ namespace BlazorEcommerce.Server.Controllers
 	[ApiController]
 	public class ProductController : ControllerBase
 	{
+		private static List<Product> Products = new List<Product>
+		{
+			new Product {
+				Id = 1,
+				Title = "The Hitchhiker's Guide to the Galaxy",
+				Description = "The Hitchhiker's Guide to the Galaxy is a comedy science fiction franchise created by Douglas Adams.",
+				ImageUrl = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
+				Price = 9.99m,
+			},
+			new Product {
+				Id = 2,
+				Title = "Ready Player One",
+				Description = "Ready Player One is a 2011 science fiction novel, and the debut novel of American author Ernest Cline.",
+				ImageUrl = "https://upload.wikimedia.org/wikipedia/en/a/a4/Ready_Player_One_cover.jpg",
+				Price = 7.99m,
+			},
+			new Product {
+				Id = 3,
+				Title = "Nineteen Eighty-Four",
+				Description = "Nineteen Eighty-Four (also published as 1984) is a dystopian social science fiction novel and cautionary tale by English writer George Orwell.",
+				ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/c/c3/1984first.jpg",
+				Price = 6.99m,
+			},
+
+		};
+
+		[HttpGet]
+		public async Task<IActionResult> GetProducts()
+		{
+			return Ok(Products);
+		}
 	}
 }
