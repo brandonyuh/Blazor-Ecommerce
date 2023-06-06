afc80e19447450bc42f061a47c40b416e72ed7f7
Brandon <brandonyuh@gmail.com>
2023-06-06 12:04:08 -0700

    Add Categories for Products

--- BlazorEcommerce/Client/Services/ProductService/IProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/IProductService.cs
@@ -3,8 +3,9 @@ namespace BlazorEcommerce.Client.Services.ProductService
 {
 	public interface IProductService
 	{
+		event Action ProductsChanged;
 		public List<Product> Products { get; set; }
-		public Task GetProducts();
+		public Task GetProducts(string? categoryUrl = null);
 		public Task<ServiceResponse<Product>> GetProduct(int productId);
 	}
 }
