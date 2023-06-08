5da538a4f5e27674217a7a07fb4d18f2834cdf1f
Brandon <brandonyuh@gmail.com>
2023-06-07 23:01:48 -0700

    Add pagination to the client

--- BlazorEcommerce/Client/Services/ProductService/IProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/IProductService.cs
@@ -6,10 +6,14 @@ namespace BlazorEcommerce.Client.Services.ProductService
 		event Action ProductsChanged;
 		public List<Product> Products { get; set; }
 		string Message { get; set; }
+		int CurrentPage { get; set; }
+		int PageCount { get; set; }
+		string LastSearchText { get; set; }
+
 		public Task GetProducts(string? categoryUrl = null);
 		public Task<ServiceResponse<Product>> GetProduct(int productId);
 
-		public Task SearchProducts(string searchText);
+		public Task SearchProducts(string searchText, int page);
 		public Task<List<string>> GetProductSearchSuggestions(string searchText);
 
 	}
--- BlazorEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -11,6 +11,10 @@
 		public List<Product> Products { get; set; } = new List<Product>();
 		public string Message { get; set; } = "Loading products...";
 
+		public int CurrentPage { get; set; } = 1;
+		public int PageCount { get; set; } = 0;
+		public string LastSearchText { get; set; } = string.Empty;
+
 		public event Action ProductsChanged;
 
 		public async Task<ServiceResponse<Product>> GetProduct(int productId)
@@ -27,6 +31,14 @@
 			if (result != null && result.Success)
 				Products = result.Data;
 
+			CurrentPage = 1;
+			PageCount = 0;
+
+			if (Products.Count == 0)
+			{
+				Message = "No products found.";
+			}
+
 			ProductsChanged?.Invoke();
 
 
@@ -39,14 +51,17 @@
 			return result.Data;
 		}
 
-		public async Task SearchProducts(string searchText)
+		public async Task SearchProducts(string searchText, int page)
 		{
-			var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
-			if(result != null && result.Data != null)
+			LastSearchText = searchText;
+			var result = await _http.GetFromJsonAsync<ServiceResponse<ProductSearchResult>>($"api/product/search/{searchText}/{page}");
+			if (result != null && result.Data != null)
 			{
-				Products = result.Data;
+				Products = result.Data.Products;
+				CurrentPage = result.Data.CurrentPage;
+				PageCount = result.Data.Pages;
 			}
-			if(Products.Count == 0)
+			if (Products.Count == 0)
 			{
 				Message = "No products found.";
 			}
