07774cc94cf5f292c73afc917c9b70ab0990eef2
Brandon <brandonyuh@gmail.com>
2023-06-17 23:27:42 -0700

    Implement show entire order details for an order

--- BlazorEcommerce/Server/Controllers/OrderController.cs
+++ BlazorEcommerce/Server/Controllers/OrderController.cs
@@ -27,5 +27,12 @@ namespace BlazorEcommerce.Server.Controllers
             var result = await _orderService.GetOrders();
             return Ok(result);
         }
-    }
+
+		[HttpGet("{orderId}")]
+		public async Task<ActionResult<ServiceResponse<OrderDetailsResponse>>> GetOrderDetails(int orderId)
+		{
+			var result = await _orderService.GetOrdersDetails(orderId);
+			return Ok(result);
+		}
+	}
 }
