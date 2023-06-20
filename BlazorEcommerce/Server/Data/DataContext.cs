021a78945bb2575835b53d5a070f0cc70386bf0b
Brandon <brandonyuh@gmail.com>
2023-06-20 10:41:06 -0700

    Add address to database

--- BlazorEcommerce/Server/Data/DataContext.cs
+++ BlazorEcommerce/Server/Data/DataContext.cs
@@ -11,9 +11,9 @@
 		{
 			modelBuilder.Entity<CartItem>().HasKey(ci => new { ci.UserId, ci.ProductId, ci.ProductTypeId });
 			modelBuilder.Entity<ProductVariant>().HasKey(pv => new { pv.ProductId, pv.ProductTypeId });
-            modelBuilder.Entity<OrderItem>().HasKey(oi => new { oi.OrderId, oi.ProductId, oi.ProductTypeId });
+			modelBuilder.Entity<OrderItem>().HasKey(oi => new { oi.OrderId, oi.ProductId, oi.ProductTypeId });
 
-            modelBuilder.Entity<ProductType>().HasData(
+			modelBuilder.Entity<ProductType>().HasData(
 					new ProductType { Id = 1, Name = "Default" },
 					new ProductType { Id = 2, Name = "Paperback" },
 					new ProductType { Id = 3, Name = "E-Book" },
@@ -263,6 +263,7 @@
 		public DbSet<User> Users { get; set; }
 		public DbSet<CartItem> CartItems { get; set; }
 		public DbSet<Order> Orders { get; set; }
-		public DbSet<OrderItem> OrderItems { get; set;}
+		public DbSet<OrderItem> OrderItems { get; set; }
+		public DbSet<Address> Addresses { get; set; }
 	}
 }
