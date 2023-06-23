c897bdecdf71ab6892611a5342ed27defe3da57d
Brandon <brandonyuh@gmail.com>
2023-06-22 18:45:43 -0700

    Implement only show products and variants if they are visible and not deleted

--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -45,9 +45,9 @@ namespace BlazorEcommerce.Server.Services.ProductService
 		{
 			var response = new ServiceResponse<Product>();
 			var product = await _context.Products
-				.Include(p => p.Variants)
+				.Include(p => p.Variants.Where(v => v.Visible && !v.Deleted))
 				.ThenInclude(v => v.ProductType)
-				.FirstOrDefaultAsync(p => p.Id == productId);
+				.FirstOrDefaultAsync(p => p.Id == productId && !p.Deleted && p.Visible);
 			if (product == null)
 			{
 				response.Success = false;
@@ -64,7 +64,10 @@ namespace BlazorEcommerce.Server.Services.ProductService
 		{
 			var response = new ServiceResponse<List<Product>>()
 			{
-				Data = await _context.Products.Include(p => p.Variants).ToListAsync()
+				Data = await _context.Products
+				.Where(p => !p.Deleted && p.Visible)
+				.Include(p => p.Variants.Where(p => !p.Deleted && p.Visible))
+				.ToListAsync()
 			};
 			return response;
 		}
@@ -74,8 +77,8 @@ namespace BlazorEcommerce.Server.Services.ProductService
 			var response = new ServiceResponse<List<Product>>
 			{
 				Data = await _context.Products
-								.Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
-								.Include(p => p.Variants)
+								.Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()) && p.Visible && !p.Deleted)
+								.Include(p => p.Variants.Where(v => !v.Deleted && v.Visible))
 								.ToListAsync()
 			};
 			return response;
@@ -84,9 +87,8 @@ namespace BlazorEcommerce.Server.Services.ProductService
 		private Task<List<Product>> FindProductsBySearchText(string searchText)
 		{
 			return _context.Products
-					.Where(p => p.Title.ToLower().Contains(searchText.ToLower())
-					||
-					p.Description.Contains(searchText.ToLower()))
+					.Where(p => p.Title.ToLower().Contains(searchText.ToLower()) ||
+					p.Description.Contains(searchText.ToLower()) && p.Visible && !p.Deleted)
 					.Include(p => p.Variants)
 					.ToListAsync();
 		}
@@ -96,9 +98,8 @@ namespace BlazorEcommerce.Server.Services.ProductService
 			var pageResults = 2f;
 			var pageCount = Math.Ceiling((await FindProductsBySearchText(searchText)).Count / pageResults);
 			var products = await _context.Products
-					.Where(p => p.Title.ToLower().Contains(searchText.ToLower())
-					||
-					p.Description.Contains(searchText.ToLower()))
+					.Where(p => p.Title.ToLower().Contains(searchText.ToLower()) ||
+					p.Description.Contains(searchText.ToLower()) && p.Visible && !p.Deleted)
 					.Include(p => p.Variants)
 					.Skip((page - 1) * (int)pageResults)
 					.Take((int)pageResults)
@@ -121,8 +122,8 @@ namespace BlazorEcommerce.Server.Services.ProductService
 			var response = new ServiceResponse<List<Product>>
 			{
 				Data = await _context.Products
-				.Where(p => p.Featured)
-				.Include(p => p.Variants)
+				.Where(p => p.Featured && p.Visible && !p.Deleted)
+				.Include(p => p.Variants.Where(v => !v.Deleted && v.Visible))
 				.ToListAsync()
 			};
 			return response;
